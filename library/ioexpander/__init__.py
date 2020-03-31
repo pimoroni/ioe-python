7f5eff3dbc500b530725dc7e8199ca0d2e5bab97
Phil Howard <phil@gadgetoid.com>
2020-03-31 11:05:22 +0100

    Fixes and linting

--- library/ioexpander/__init__.py
+++ library/ioexpander/__init__.py
@@ -17,141 +17,141 @@ REG_INT = 0xf9
 BIT_INT_TRIGD = 0
 BIT_INT_OUT_EN = 1
 
-REG_P0 = 0x40 
-REG_SP = 0x41       # Read only 
-REG_DPL = 0x42      # Read only 
-REG_DPH = 0x43      # Read only 
-REG_RCTRIM0 = 0x44  # Read only 
-REG_RCTRIM1 = 0x45  # Read only 
-REG_RWK = 0x46 
-REG_PCON = 0x47     # Read only 
-REG_TCON = 0x48 
-REG_TMOD = 0x49 
-REG_TL0 = 0x4a 
-REG_TL1 = 0x4b 
-REG_TH0 = 0x4c 
-REG_TH1 = 0x4d 
-REG_CKCON = 0x4e 
-REG_WKCON = 0x4f    # Read only 
-REG_P1 = 0x50 
-REG_SFRS = 0x51     # TA protected # Read only 
-REG_CAPCON0 = 0x52 
-REG_CAPCON1 = 0x53 
-REG_CAPCON2 = 0x54 
-REG_CKDIV = 0x55 
-REG_CKSWT = 0x56    # TA protected # Read only 
-REG_CKEN = 0x57     # TA protected # Read only 
-REG_SCON = 0x58 
-REG_SBUF = 0x59 
-REG_SBUF_1 = 0x5a 
-REG_EIE = 0x5b      # Read only 
-REG_EIE1 = 0x5c     # Read only 
-REG_CHPCON = 0x5f   # TA protected # Read only 
-REG_P2 = 0x60 
-REG_AUXR1 = 0x62 
-REG_BODCON0 = 0x63  # TA protected 
-REG_IAPTRG = 0x64   # TA protected # Read only 
-REG_IAPUEN = 0x65   # TA protected # Read only 
-REG_IAPAL = 0x66    # Read only 
-REG_IAPAH = 0x67    # Read only 
-REG_IE = 0x68       # Read only 
-REG_SADDR = 0x69 
-REG_WDCON = 0x6a    # TA protected 
-REG_BODCON1 = 0x6b  # TA protected 
-REG_P3M1 = 0x6c 
-REG_P3S = 0xc0      # Page 1 # Reassigned from 0x6c to avoid collision 
-REG_P3M2 = 0x6d 
-REG_P3SR = 0xc1     # Page 1 # Reassigned from 0x6d to avoid collision 
-REG_IAPFD = 0x6e    # Read only 
-REG_IAPCN = 0x6f    # Read only 
-REG_P3 = 0x70 
-REG_P0M1 = 0x71 
-REG_P0S = 0xc2      # Page 1 # Reassigned from 0x71 to avoid collision 
-REG_P0M2 = 0x72 
-REG_P0SR = 0xc3     # Page 1 # Reassigned from 0x72 to avoid collision 
-REG_P1M1 = 0x73 
-REG_P1S = 0xc4      # Page 1 # Reassigned from 0x73 to avoid collision 
-REG_P1M2 = 0x74 
-REG_P1SR = 0xc5     # Page 1 # Reassigned from 0x74 to avoid collision 
-REG_P2S = 0x75 
-REG_IPH = 0x77      # Read only 
-REG_PWMINTC = 0xc6  # Page 1 # Read only # Reassigned from 0x77 to avoid collision 
-REG_IP = 0x78       # Read only 
-REG_SADEN = 0x79 
-REG_SADEN_1 = 0x7a 
-REG_SADDR_1 = 0x7b 
-REG_I2DAT = 0x7c    # Read only 
-REG_I2STAT = 0x7d   # Read only 
-REG_I2CLK = 0x7e    # Read only 
-REG_I2TOC = 0x7f    # Read only 
-REG_I2CON = 0x80    # Read only 
-REG_I2ADDR = 0x81   # Read only 
-REG_ADCRL = 0x82 
-REG_ADCRH = 0x83 
-REG_T3CON = 0x84 
-REG_PWM4H = 0xc7    # Page 1 # Reassigned from 0x84 to avoid collision 
-REG_RL3 = 0x85 
-REG_PWM5H = 0xc8    # Page 1 # Reassigned from 0x85 to avoid collision 
-REG_RH3 = 0x86 
-REG_PIOCON1 = 0xc9  # Page 1 # Reassigned from 0x86 to avoid collision 
-REG_TA = 0x87       # Read only 
-REG_T2CON = 0x88 
-REG_T2MOD = 0x89 
-REG_RCMP2L = 0x8a 
-REG_RCMP2H = 0x8b 
-REG_TL2 = 0x8c 
-REG_PWM4L = 0xca    # Page 1 # Reassigned from 0x8c to avoid collision 
-REG_TH2 = 0x8d 
-REG_PWM5L = 0xcb    # Page 1 # Reassigned from 0x8d to avoid collision 
-REG_ADCMPL = 0x8e 
-REG_ADCMPH = 0x8f 
-REG_PSW = 0x90      # Read only 
-REG_PWMPH = 0x91 
-REG_PWM0H = 0x92 
-REG_PWM1H = 0x93 
-REG_PWM2H = 0x94 
-REG_PWM3H = 0x95 
-REG_PNP = 0x96 
-REG_FBD = 0x97 
-REG_PWMCON0 = 0x98 
-REG_PWMPL = 0x99 
-REG_PWM0L = 0x9a 
-REG_PWM1L = 0x9b 
-REG_PWM2L = 0x9c 
-REG_PWM3L = 0x9d 
-REG_PIOCON0 = 0x9e 
-REG_PWMCON1 = 0x9f 
-REG_ACC = 0xa0      # Read only 
-REG_ADCCON1 = 0xa1 
-REG_ADCCON2 = 0xa2 
-REG_ADCDLY = 0xa3 
-REG_C0L = 0xa4 
-REG_C0H = 0xa5 
-REG_C1L = 0xa6 
-REG_C1H = 0xa7 
-REG_ADCCON0 = 0xa8 
-REG_PICON = 0xa9    # Read only 
-REG_PINEN = 0xaa    # Read only 
-REG_PIPEN = 0xab    # Read only 
-REG_PIF = 0xac      # Read only 
-REG_C2L = 0xad 
-REG_C2H = 0xae 
-REG_EIP = 0xaf      # Read only 
-REG_B = 0xb0        # Read only 
-REG_CAPCON3 = 0xb1 
-REG_CAPCON4 = 0xb2 
-REG_SPCR = 0xb3 
-REG_SPCR2 = 0xcc    # Page 1 # Reassigned from 0xb3 to avoid collision 
-REG_SPSR = 0xb4 
-REG_SPDR = 0xb5 
-REG_AINDIDS = 0xb6 
-REG_EIPH = 0xb7     # Read only 
-REG_SCON_1 = 0xb8 
-REG_PDTEN = 0xb9    # TA protected 
-REG_PDTCNT = 0xba   # TA protected 
-REG_PMEN = 0xbb 
-REG_PMD = 0xbc 
-REG_EIP1 = 0xbe     # Read only 
+REG_P0 = 0x40       # protect_bits 2 # Bit addressing
+REG_SP = 0x41       # Read only
+REG_DPL = 0x42      # Read only
+REG_DPH = 0x43      # Read only
+REG_RCTRIM0 = 0x44  # Read only
+REG_RCTRIM1 = 0x45  # Read only
+REG_RWK = 0x46
+REG_PCON = 0x47     # Read only
+REG_TCON = 0x48
+REG_TMOD = 0x49
+REG_TL0 = 0x4a
+REG_TL1 = 0x4b
+REG_TH0 = 0x4c
+REG_TH1 = 0x4d
+REG_CKCON = 0x4e
+REG_WKCON = 0x4f    # Read only
+REG_P1 = 0x50       # protect_bits 3 6 # Bit addressing
+REG_SFRS = 0x51     # TA protected # Read only
+REG_CAPCON0 = 0x52
+REG_CAPCON1 = 0x53
+REG_CAPCON2 = 0x54
+REG_CKDIV = 0x55
+REG_CKSWT = 0x56    # TA protected # Read only
+REG_CKEN = 0x57     # TA protected # Read only
+REG_SCON = 0x58
+REG_SBUF = 0x59
+REG_SBUF_1 = 0x5a
+REG_EIE = 0x5b      # Read only
+REG_EIE1 = 0x5c     # Read only
+REG_CHPCON = 0x5f   # TA protected # Read only
+REG_P2 = 0x60       # Bit addressing
+REG_AUXR1 = 0x62
+REG_BODCON0 = 0x63  # TA protected
+REG_IAPTRG = 0x64   # TA protected # Read only
+REG_IAPUEN = 0x65   # TA protected # Read only
+REG_IAPAL = 0x66    # Read only
+REG_IAPAH = 0x67    # Read only
+REG_IE = 0x68       # Read only
+REG_SADDR = 0x69
+REG_WDCON = 0x6a    # TA protected
+REG_BODCON1 = 0x6b  # TA protected
+REG_P3M1 = 0x6c
+REG_P3S = 0xc0      # Page 1 # Reassigned from 0x6c to avoid collision
+REG_P3M2 = 0x6d
+REG_P3SR = 0xc1     # Page 1 # Reassigned from 0x6d to avoid collision
+REG_IAPFD = 0x6e    # Read only
+REG_IAPCN = 0x6f    # Read only
+REG_P3 = 0x70       # Bit addressing
+REG_P0M1 = 0x71     # protect_bits  2
+REG_P0S = 0xc2      # Page 1 # Reassigned from 0x71 to avoid collision
+REG_P0M2 = 0x72     # protect_bits  2
+REG_P0SR = 0xc3     # Page 1 # Reassigned from 0x72 to avoid collision
+REG_P1M1 = 0x73     # protect_bits  3 6
+REG_P1S = 0xc4      # Page 1 # Reassigned from 0x73 to avoid collision
+REG_P1M2 = 0x74     # protect_bits  3 6
+REG_P1SR = 0xc5     # Page 1 # Reassigned from 0x74 to avoid collision
+REG_P2S = 0x75
+REG_IPH = 0x77      # Read only
+REG_PWMINTC = 0xc6  # Page 1 # Read only # Reassigned from 0x77 to avoid collision
+REG_IP = 0x78       # Read only
+REG_SADEN = 0x79
+REG_SADEN_1 = 0x7a
+REG_SADDR_1 = 0x7b
+REG_I2DAT = 0x7c    # Read only
+REG_I2STAT = 0x7d   # Read only
+REG_I2CLK = 0x7e    # Read only
+REG_I2TOC = 0x7f    # Read only
+REG_I2CON = 0x80    # Read only
+REG_I2ADDR = 0x81   # Read only
+REG_ADCRL = 0x82
+REG_ADCRH = 0x83
+REG_T3CON = 0x84
+REG_PWM4H = 0xc7    # Page 1 # Reassigned from 0x84 to avoid collision
+REG_RL3 = 0x85
+REG_PWM5H = 0xc8    # Page 1 # Reassigned from 0x85 to avoid collision
+REG_RH3 = 0x86
+REG_PIOCON1 = 0xc9  # Page 1 # Reassigned from 0x86 to avoid collision
+REG_TA = 0x87       # Read only
+REG_T2CON = 0x88
+REG_T2MOD = 0x89
+REG_RCMP2L = 0x8a
+REG_RCMP2H = 0x8b
+REG_TL2 = 0x8c
+REG_PWM4L = 0xca    # Page 1 # Reassigned from 0x8c to avoid collision
+REG_TH2 = 0x8d
+REG_PWM5L = 0xcb    # Page 1 # Reassigned from 0x8d to avoid collision
+REG_ADCMPL = 0x8e
+REG_ADCMPH = 0x8f
+REG_PSW = 0x90      # Read only
+REG_PWMPH = 0x91
+REG_PWM0H = 0x92
+REG_PWM1H = 0x93
+REG_PWM2H = 0x94
+REG_PWM3H = 0x95
+REG_PNP = 0x96
+REG_FBD = 0x97
+REG_PWMCON0 = 0x98
+REG_PWMPL = 0x99
+REG_PWM0L = 0x9a
+REG_PWM1L = 0x9b
+REG_PWM2L = 0x9c
+REG_PWM3L = 0x9d
+REG_PIOCON0 = 0x9e
+REG_PWMCON1 = 0x9f
+REG_ACC = 0xa0      # Read only
+REG_ADCCON1 = 0xa1
+REG_ADCCON2 = 0xa2
+REG_ADCDLY = 0xa3
+REG_C0L = 0xa4
+REG_C0H = 0xa5
+REG_C1L = 0xa6
+REG_C1H = 0xa7
+REG_ADCCON0 = 0xa8
+REG_PICON = 0xa9    # Read only
+REG_PINEN = 0xaa    # Read only
+REG_PIPEN = 0xab    # Read only
+REG_PIF = 0xac      # Read only
+REG_C2L = 0xad
+REG_C2H = 0xae
+REG_EIP = 0xaf      # Read only
+REG_B = 0xb0        # Read only
+REG_CAPCON3 = 0xb1
+REG_CAPCON4 = 0xb2
+REG_SPCR = 0xb3
+REG_SPCR2 = 0xcc    # Page 1 # Reassigned from 0xb3 to avoid collision
+REG_SPSR = 0xb4
+REG_SPDR = 0xb5
+REG_AINDIDS = 0xb6
+REG_EIPH = 0xb7     # Read only
+REG_SCON_1 = 0xb8
+REG_PDTEN = 0xb9    # TA protected
+REG_PDTCNT = 0xba   # TA protected
+REG_PMEN = 0xbb
+REG_PMD = 0xbc
+REG_EIP1 = 0xbe     # Read only
 REG_EIPH1 = 0xbf    # Read only
 
 # Special mode registers, use a bit-addressing scheme to avoid
@@ -290,16 +290,16 @@ class IOE():
     def set_mode(self, pin, mode):
         if pin < 1 or pin > len(self._pins):
             raise ValueError("Pin should be in range 1-14.")
- 
+
         io_pin = self._pins[pin - 1]
         if io_pin.mode == mode:
             return
 
         gpio_mode = mode & 0b11
-        io_mode =  mode >> 2
+        io_mode = mode >> 2
 
         if io_mode != PIN_MODE_IO and mode != io_pin.type:
-            raise ValueError("Pin {} does not support {}!".format(pin, MODE_NAMES[mode]))
+            raise ValueError("Pin {} does not support {}!".format(pin, MODE_NAMES[io_mode]))
 
         io_pin.mode = mode
         if self._debug:
@@ -311,7 +311,6 @@ class IOE():
         else:
             if io_pin.type == PIN_MODE_PWM:
                 self.clr_bit(io_pin.reg_iopwm, io_pin.channel)
-
 
         pm1 = self.i2c_read8(io_pin.reg_m1)
         pm2 = self.i2c_read8(io_pin.reg_m2)
@@ -329,11 +328,11 @@ class IOE():
 
     def input(self, pin, adc_timeout=1):
         """Read the IO pin state.
-        
+
         Returns a 12-bit ADC reading if the pin is in ADC mode
         Returns True/False if the pin is in any other input mode
         Returns None if the pin is in PWM mode
-        
+
         """
         if pin < 1 or pin > len(self._pins):
             raise ValueError("Pin should be in range 1-14.")
@@ -372,7 +371,7 @@ class IOE():
 
         # Fall-through for PWM mode
         return None
-    
+
     def output(self, pin, value):
         if pin < 1 or pin > len(self._pins):
             raise ValueError("Pin should be in range 1-14.")
@@ -384,10 +383,10 @@ class IOE():
                 print("Outputting PWM to pin: {pin}".format(pin=pin))
             self.i2c_write8(io_pin.reg_pwml, value & 0xff)
             self.i2c_write8(io_pin.reg_pwmh, value >> 8)
-            self.set_bit(REG_PWMCON0, 6)  # Set the "LOAD" bit of PWMCON0
-                                          # Loads new period and duty registers into buffer
+            # Load new period and duty registers into buffer
+            self.set_bit(REG_PWMCON0, 6)    # Set the "LOAD" bit of PWMCON0
             while self.get_bit(REG_PWMCON0, 6):
-                 time.sleep(0.001)         # Wait for "LOAD" to complete
+                time.sleep(0.001)           # Wait for "LOAD" to complete
 
         elif io_pin.mode != PIN_MODE_IN:
             if value == LOW:
@@ -398,32 +397,3 @@ class IOE():
                 if self._debug:
                     print("Outputting HIGH to pin: {pin}".format(pin=pin, value=value))
                 self.set_bit(io_pin.reg_p, io_pin.pin)
-
-
-def test_cycles():
-    ioe = IOE()
-
-    #ioe.set_mode(1, PWM)
-    #ioe.set_mode(9, OUT)
-    ioe.set_mode(4, OUT)
-    #ioe.set_mode(14, ADC)
-
-    #ioe.output(1, 1000)
-
-    out_value = LOW
-
-    while True:
-        a = ioe.input(4)
-        #a = ioe.input(3)
-        #b = ioe.input(14)
-
-        ioe.output(4, out_value)
-        out_value = not out_value
-
-        #print("3: {a} 14: {b}".format(a=a, b=b))
-
-        time.sleep(1.0)
-
-
-if __name__ == "__main__":
-    test_cycles()
