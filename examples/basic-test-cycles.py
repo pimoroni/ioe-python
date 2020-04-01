e85bbbb8f20539e314e118fa4631c07bdbb3fbb2
Phil Howard <phil@gadgetoid.com>
2020-04-01 11:09:43 +0100

    Tidy up test cycles

--- examples/basic-test-cycles.py
+++ examples/basic-test-cycles.py
@@ -1,26 +1,27 @@
-from ioexpander import IOE, OUT, LOW, HIGH
+import time
+import ioexpander as io
+
 
 def test_cycles():
-    ioe = IOE()
+    ioe = io.IOE()
 
-    # ioe.set_mode(1, PWM)
-    # ioe.set_mode(9, OUT)
-    ioe.set_mode(4, OUT)
-    # ioe.set_mode(14, ADC)
+    # ioe.set_mode(1, io.PWM)
+    # ioe.set_mode(9, io.OUT)
+    ioe.set_mode(4, io.OUT)
+    # ioe.set_mode(14, io.ADC)
 
     # ioe.output(1, 1000)
 
-    out_value = LOW
+    out_value = io.LOW
 
     while True:
-        a = ioe.input(4)
-        # a = ioe.input(3)
-        # b = ioe.input(14)
+        a = ioe.input(3)
+        b = ioe.input(14)
 
         ioe.output(4, out_value)
         out_value = not out_value
 
-        # print("3: {a} 14: {b}".format(a=a, b=b))
+        print("3: {a} 14: {b}".format(a=a, b=b))
 
         time.sleep(1.0)
 
