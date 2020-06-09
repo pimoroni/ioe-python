64cd649eb3a8fdefb3147619fcefc2f27e85654b
Phil Howard <phil@gadgetoid.com>
2020-06-09 14:45:59 +0100

    Update example to use new features

--- examples/rotary.py
+++ examples/rotary.py
@@ -25,17 +25,7 @@ ioe = io.IOE(i2c_addr=0x18, interrupt_pin=4)
 
 ioe.enable_interrupt_out(pin_swap=True)
 
-ioe.set_mode(POT_ENC_A, io.PIN_MODE_PU)
-ioe.set_mode(POT_ENC_B, io.PIN_MODE_PU)
-ioe.set_mode(POT_ENC_C, io.PIN_MODE_OD)
-ioe.output(POT_ENC_C, 0)
-
-ioe.set_bit(io.REG_P0S, 5)  # Set P0.5 = A as schmitt trigger input
-ioe.set_bit(io.REG_P1S, 2)  # Set P1.2 = B as schmitt trigger input
-
-ioe.i2c_write8(io.REG_ENC_1_CFG, POT_ENC_A | (POT_ENC_B << 4))
-ioe.set_bit(io.REG_ENC_EN, io.BIT_ENC_MICROSTEP_1)
-ioe.set_bit(io.REG_ENC_EN, io.BIT_ENC_EN_1)
+ioe.setup_rotary_encoder(1, POT_ENC_A, POT_ENC_B, pin_c=POT_ENC_C)
 
 ioe.set_pwm_period(PERIOD)
 ioe.set_pwm_control(divider=2)  # PWM as fast as we can to avoid LED flicker
@@ -47,18 +37,18 @@ ioe.set_mode(PIN_BLUE, io.PWM, invert=True)
 print("Running LED with {} brightness steps.".format(int(PERIOD * BRIGHTNESS)))
 
 count = 0
+r, g, b, = 0, 0, 0
 
 while True:
     if ioe.get_interrupt():
-        count = ioe.i2c_read8(io.REG_ENC_1_COUNT)
-        if count & 0b10000000:
-            count -= 255
+        count = ioe.read_rotary_encoder(1)
         ioe.clear_interrupt()
-    h = time.time() / 10.0
-    r, g, b = [int(c * PERIOD * BRIGHTNESS) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
-    ioe.output(PIN_RED, r)
-    ioe.output(PIN_GREEN, g)
-    ioe.output(PIN_BLUE, b)
+    else:
+        h = (count % 360) / 360.0
+        r, g, b = [int(c * PERIOD * BRIGHTNESS) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        ioe.output(PIN_RED, r)
+        ioe.output(PIN_GREEN, g)
+        ioe.output(PIN_BLUE, b)
 
     print(count, r, g, b)
 
