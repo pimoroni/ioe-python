806c1bef7a0a5c1e96204b3f9486ca09bff572b6
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-02-10 10:42:01 +0000

    Update sioe registers to latest mapping

--- library/ioexpander/sioe_regs.py
+++ library/ioexpander/sioe_regs.py
@@ -42,38 +42,29 @@ class REGS:
     REG_FLASH_PAGE = 0xF0
     REG_DEBUG = 0xF8
 
-    REG_P0 = 0x40 # Bit addressing
+    REG_P0 = 0x7B # Bit addressing
 
 
 
 
 
-    REG_RWKL = 0x46
-    REG_TCON = 0x48
-    REG_TMOD = 0x49
-    REG_TL0 = 0x4a
-    REG_TL1 = 0x4b
+    REG_RWKL = 0x5b
+    REG_TCON = 0x55
+    REG_TMOD = 0x56
+    REG_TL0 = 0x4b
+    REG_TL1 = 0x4d
     REG_TH0 = 0x4c
-    REG_TH1 = 0x4d
-    REG_CKCON = 0x4e
-    REG_WKCON = 0x4f
-    REG_P1 = 0x50 # Bit addressing
+    REG_TH1 = 0x4e
+    REG_CKCON = 0x4a
+    REG_WKCON = 0x5a
+    REG_P1 = 0x7c # Bit addressing
 
-    REG_CAPCON0 = 0x52
-    REG_CAPCON1 = 0x53
-    REG_CAPCON2 = 0x54
-    REG_CKDIV = 0x55
-    REG_CKSWT = 0x56 # TA protected
-    REG_CKEN = 0x57 # TA protected
-
-
-
-
-
-
-
-    REG_P2 = 0x60 # Bit addressing
-    REG_AUXR1 = 0x62
+    REG_CAPCON0 = 0x60
+    REG_CAPCON1 = 0x61
+    REG_CAPCON2 = 0x62
+    REG_CKDIV = 0x47
+    REG_CKSWT = 0x48 # TA protected
+    REG_CKEN = 0x49 # TA protected
 
 
 
@@ -81,128 +72,137 @@ class REGS:
 
 
 
-    REG_WDCON = 0x6a # TA protected
+    REG_7D = 0x60 # Bit addressing
+    REG_AUXR1 = 0x41
 
-    REG_P3M1 = 0x6c
-    REG_P3M2 = 0x6d
-    REG_P3 = 0x70 # Bit addressing
-    REG_P0M1 = 0x71
-    REG_P0M2 = 0x72
-    REG_P1M1 = 0x73
-    REG_P1M2 = 0x74
-    REG_ADCRL = 0x82
-    REG_ADCRH = 0x83
-    REG_T3CON = 0x84
-    REG_RL3 = 0x85
-    REG_RH3 = 0x86
-    REG_T2CON = 0x88
-    REG_T2MOD = 0x89
-    REG_RCMP2L = 0x8a
-    REG_RCMP2H = 0x8b
-    REG_TL2 = 0x8c
-    REG_TH2 = 0x8d
-    REG_ADCMPL = 0x8e
-    REG_ADCMPH = 0x8f
-    REG_PWM0PH = 0x91
-    REG_PWM0C0H = 0x92
-    REG_PWM0C1H = 0x93
-    REG_PWM0C2H = 0x94
-    REG_PWM0C3H = 0x95
-    REG_PNP = 0x96
-    REG_PWM0FBD = 0x97
-    REG_PWM0CON0 = 0x98
-    REG_PWM0PL = 0x99
-    REG_PWM0C0L = 0x9a
-    REG_PWM0C1L = 0x9b
-    REG_PWM0C2L = 0x9c
-    REG_PWM0C3L = 0x9d
-    REG_PIOCON0 = 0x9e
-    REG_PWM0CON1 = 0x9f
-    REG_ADCCON1 = 0xa1
-    REG_ADCCON2 = 0xa2
-    REG_ADCDLY = 0xa3
-    REG_C0L = 0xa4
-    REG_C0H = 0xa5
-    REG_C1L = 0xa6
-    REG_C1H = 0xa7
-    REG_ADCCON0 = 0xa8
-    REG_C2L = 0xad
-    REG_C2H = 0xae
-    REG_CAPCON3 = 0xb1
-    REG_CAPCON4 = 0xb2
-    REG_SPCR = 0xb3
-    REG_SPSR = 0xb4
-    REG_SPDR = 0xb5
-    REG_AINDIDS0 = 0xb6
-    REG_PWM0DTEN = 0xb9 # TA protected
-    REG_PWM0DTCNT = 0xba # TA protected
-    REG_PWM0MEN = 0xbb
-    REG_PWM0MD = 0xbc
-    REG_P3S = 0x41
-    REG_P3SR = 0x42
-    REG_P0S = 0x43
-    REG_P0SR = 0x44
-    REG_P1S = 0x45
-    REG_P1SR = 0x47
-    REG_PWM0C4H = 0x51
-    REG_PWM0C5H = 0x58
-    REG_PIOCON1 = 0x59
-    REG_PWM0C4L = 0x5a
-    REG_PWM0C5L = 0x5b
-    REG_SPCR2 = 0x5c
-    REG_ADCBAL = 0x5d
-    REG_ADCBAH = 0x5e
-    REG_ADCCON3 = 0x5f
-    REG_P2M1 = 0x61
-    REG_P2M2 = 0x63
-    REG_P2SR = 0x64
-    REG_P2S = 0x65
-    REG_ADCSN = 0x66
-    REG_ADCCN = 0x67
-    REG_ADCSR = 0x68
-    REG_P0UP = 0x69
-    REG_P1UP = 0x6b
-    REG_P2UP = 0x6e
-    REG_P3UP = 0x6f
-    REG_RWKH = 0x75
-    REG_AINDIDS1 = 0x76
-    REG_P0DW = 0x77
-    REG_P1DW = 0x78
-    REG_P2DW = 0x79
-    REG_P3DW = 0x7a
-    REG_AUXR4 = 0x7b
-    REG_AUXR5 = 0x7c
-    REG_AUXR7 = 0x7d
-    REG_AUXR8 = 0x7e
-    REG_PWM1PH = 0x7f
-    REG_PWM1C0H = 0x80
-    REG_PWM1C1H = 0x81
-    REG_PWM1MD = 0x87
-    REG_PWM1MEN = 0x90
-    REG_PWM1PL = 0xa0
-    REG_PWM1C0L = 0xa9
-    REG_PWM1C1L = 0xaa
-    REG_PWM1CON0 = 0xab
-    REG_PWM1CON1 = 0xac
-    REG_PIOCON2 = 0xaf
-    REG_PWM2PH = 0xb0
-    REG_PWM2C0H = 0xb7
-    REG_PWM2C1H = 0xb8
-    REG_PWM2MD = 0xbd
-    REG_PWM2MEN = 0xbe
-    REG_PWM2PL = 0xbf
-    REG_PWM2C0L = 0xc0
-    REG_PWM2C1L = 0xc1
+
+
+
+
+
+
+    REG_WDCON = 0x5d # TA protected
+
+    REG_P3M1 = 0x91
+    REG_P3M2 = 0x92
+    REG_P3 = 0x7e # Bit addressing
+    REG_P0M1 = 0x7f
+    REG_P0M2 = 0x80
+    REG_P1M1 = 0x85
+    REG_P1M2 = 0x86
+    REG_ADCRL = 0x6b
+    REG_ADCRH = 0x6c
+    REG_T3CON = 0x59
+    REG_RL3 = 0x53
+    REG_RH3 = 0x54
+    REG_T2CON = 0x57
+    REG_T2MOD = 0x58
+    REG_RCMP2L = 0x51
+    REG_RCMP2H = 0x52
+    REG_TL2 = 0x4f
+    REG_TH2 = 0x50
+    REG_ADCMPL = 0x78
+    REG_ADCMPH = 0x79
+    REG_PWM0PH = 0x9d
+    REG_PWM0C0H = 0x9f
+    REG_PWM0C1H = 0xa1
+    REG_PWM0C2H = 0xa3
+    REG_PWM0C3H = 0xa5
+    REG_PNP = 0xb1
+    REG_PWM0FBD = 0xae
+    REG_PWM0CON0 = 0xaa
+    REG_PWM0PL = 0x9c
+    REG_PWM0C0L = 0x9e
+    REG_PWM0C1L = 0xa0
+    REG_PWM0C2L = 0xa2
+    REG_PWM0C3L = 0xa4
+    REG_PIOCON0 = 0x99
+    REG_PWM0CON1 = 0xab
+    REG_ADCCON1 = 0x70
+    REG_ADCCON2 = 0x71
+    REG_ADCDLY = 0x7a
+    REG_C0L = 0x65
+    REG_C0H = 0x66
+    REG_C1L = 0x67
+    REG_C1H = 0x68
+    REG_ADCCON0 = 0x6f
+    REG_C2L = 0x69
+    REG_C2H = 0x6a
+    REG_CAPCON3 = 0x63
+    REG_CAPCON4 = 0x64
+    REG_SPCR = 0x42
+    REG_SPSR = 0x44
+    REG_SPDR = 0x45
+    REG_AINDIDS0 = 0x6d
+    REG_PWM0DTEN = 0xaf # TA protected
+    REG_PWM0DTCNT = 0xb0 # TA protected
+    REG_PWM0MEN = 0xad
+    REG_PWM0MD = 0xac
+    REG_P3S = 0x95
+    REG_P3SR = 0x96
+    REG_P0S = 0x83
+    REG_P0SR = 0x84
+    REG_P1S = 0x89
+    REG_P1SR = 0x8a
+    REG_PWM0C4H = 0xa7
+    REG_PWM0C5H = 0xa9
+    REG_PIOCON1 = 0x9a
+    REG_PWM0C4L = 0xa6
+    REG_PWM0C5L = 0xa8
+    REG_SPCR2 = 0x43
+    REG_ADCBAL = 0x73
+    REG_ADCBAH = 0x74
+    REG_ADCCON3 = 0x72
+    REG_P2M1 = 0x8b
+    REG_P2M2 = 0x8c
+    REG_P2SR = 0x90
+    REG_P2S = 0x8f
+    REG_ADCSN = 0x75
+    REG_ADCCN = 0x76
+    REG_ADCSR = 0x77
+    REG_P0UP = 0x81
+    REG_P1UP = 0x87
+    REG_P2UP = 0x8d
+    REG_P3UP = 0x93
+    REG_RWKH = 0x5c
+    REG_AINDIDS1 = 0x6e
+    REG_P0DW = 0x82
+    REG_P1DW = 0x88
+    REG_P2DW = 0x8e
+    REG_P3DW = 0x94
+    REG_AUXR4 = 0x97
+    REG_AUXR5 = 0x98
+    REG_AUXR7 = 0x46
+    REG_AUXR8 = 0x5f
+    REG_PWM1PH = 0xb3
+    REG_PWM1C0H = 0xb5
+    REG_PWM1C1H = 0xb7
+    REG_PWM1MD = 0xba
+    REG_PWM1MEN = 0xbb
+    REG_PWM1PL = 0xb2
+    REG_PWM1C0L = 0xb4
+    REG_PWM1C1L = 0xb6
+    REG_PWM1CON0 = 0xb8
+    REG_PWM1CON1 = 0xb9
+    REG_PIOCON2 = 0x9b
+    REG_PWM2PH = 0xbd
+    REG_PWM2C0H = 0xbf
+    REG_PWM2C1H = 0xc1
+    REG_PWM2MD = 0xc4
+    REG_PWM2MEN = 0xc5
+    REG_PWM2PL = 0xbc
+    REG_PWM2C0L = 0xbe
+    REG_PWM2C1L = 0xc0
     REG_PWM2CON0 = 0xc2
     REG_PWM2CON1 = 0xc3
-    REG_PWM3PH = 0xc4
-    REG_PWM3C0H = 0xc5
-    REG_PWM3C1H = 0xc6
-    REG_PWM3MD = 0xc7
-    REG_PWM3MEN = 0xc8
-    REG_PWM3PL = 0xc9
-    REG_PWM3C0L = 0xca
-    REG_PWM3C1L = 0xcb
+    REG_PWM3PH = 0xc7
+    REG_PWM3C0H = 0xc9
+    REG_PWM3C1H = 0xcb
+    REG_PWM3MD = 0xce
+    REG_PWM3MEN = 0xcf
+    REG_PWM3PL = 0xc6
+    REG_PWM3C0L = 0xc8
+    REG_PWM3C1L = 0xca
     REG_PWM3CON0 = 0xcc
     REG_PWM3CON1 = 0xcd
 
