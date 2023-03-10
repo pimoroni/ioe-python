2891cdf1cf0b7d8454d1125d94c4766bb12d8f61
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-03-10 12:25:21 +0000

    Update version to 0.0.4

--- library/ioexpander/__init__.py
+++ library/ioexpander/__init__.py
@@ -5,7 +5,7 @@ from smbus2 import SMBus, i2c_msg
 from . import sioe_regs
 from . import ioe_regs
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 
 # These values encode our desired pin function: IO, ADC, PWM
