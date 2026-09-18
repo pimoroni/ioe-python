4db3158a764bcfa09292429b6b2da51f298a4967
Phil Howard <github@gadgetoid.com>
2026-09-18 15:46:42 +0100

    Adopt tag-derived versioning and PyPI trusted publishing

--- ioexpander/__init__.py
+++ ioexpander/__init__.py
@@ -1,10 +1,14 @@
 import time
+from importlib.metadata import PackageNotFoundError, version
 
 from smbus2 import SMBus, i2c_msg
 
 from . import ioe_regs, sioe_regs
 
-__version__ = "1.0.1"
+try:
+    __version__ = version("ioexpander")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 
 # These values encode our desired pin function: IO, ADC, PWM
